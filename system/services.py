3f7643893c3d1f547775f39a7949c95783ae739f
maysu1914 <mustafa.aysu@hotmail.com>
2022-01-04 20:41:01 +0300

    feat(system.services): add new service for deleted functions

--- system/services.py
+++ system/services.py
@@ -46,3 +46,47 @@ class DirectoryService:
 
     def is_empty(self, path):
         return self._is_empty(path)
+
+
+class WindowsImageService:
+    executor = ThreadPoolExecutor()
+
+    def __init__(self, *args, **kwargs):
+        self.detail_files = ["\\sources\\install.esd", "\\sources\\install.wim", "\\sources\\boot.wim"]
+        self.media_files = ['sources', 'support', 'efi', 'boot', 'upgrade', 'autorun.inf', 'bootmgr', 'bootmgr.efi',
+                            'setup.exe']
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def _get_source_filename(self, path):
+        for filename in self.detail_files:
+            if os.path.isfile(f"{path}{filename}"):
+                return filename
+        return
+
+    def is_windows_image(self, path):
+        return any(os.path.isfile(f"{path}{file}") for file in self.detail_files)
+
+    def _get_windows_image_detail(self, path):
+        if not self.is_windows_image(path):
+            return
+        detail_filename = self._get_source_filename(path)
+        command = f"""dism /Get-WimInfo /WimFile:"{path}{detail_filename}" /index:1"""
+        output = subprocess.getoutput(command)
+        if string_to_dict(output).get("Error"):
+            raise AdminPrivilegesException(output, "Run the application with admin privileges.")
+        return output
+
+    def get_windows_image_details(self, paths):
+        details = []
+        path_and_threads = []
+        if not isinstance(paths, list):
+            paths = [paths]
+        for path in paths:
+            path_and_threads.append({path: self.executor.submit(self._get_windows_image_detail, path)})
+        for path_and_thread in path_and_threads:
+            (path, thread) = path_and_thread.popitem()
+            detail = thread.result()
+            if detail:
+                details.append({path: string_to_dict(detail, remove_last_line=True)})
+        return details
